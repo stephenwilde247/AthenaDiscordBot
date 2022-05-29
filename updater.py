b53943ef0e03a98c805807302fdfbdf8bf554c6b
stephen <myself@stephenwilde.net>
2022-05-29 17:19:52 +0100

    adding updater

--- updater.py
+++ updater.py
@@ -5,23 +5,27 @@ import subprocess
 file = open("VersionNumber.txt")
 Version = (file.read())
 file.close()
+updateNow = False
 
 def updateVersion():
     http = urllib3.PoolManager()
     response = http.request('GET', 'https://wilde247.com/discordBot/version.txt')
     data = response.data.decode('utf-8')
-    print (data)
 
     if data == Version:
-        print("It matches continuing with program....")
-        print(data)
+        print(f"Your version is up to date: {data}")
     else:
-        print("It don't match.. getting update......")
-        url = 'https://github.com/stephenwilde247/AthenaDiscordBot/archive/refs/heads/master.zip'
-        urllib.request.urlretrieve(url, 'AthenaDiscordBot-master.zip')
-        subprocess.call('unzip AthenaDiscordBot-master.zip')
-        print("Closing Program Please Reload")
-        print(data)
-        exit(0)
+        print("Your version is not up to date do you want to update now?")
+        yesno = input('Type "yes" to update!: ')
+        if yesno == 'yes':
+            url = 'https://github.com/stephenwilde247/AthenaDiscordBot/archive/refs/heads/master.zip'
+            urllib.request.urlretrieve(url, 'AthenaDiscordBot-master.zip')
+            subprocess.call('unzip AthenaDiscordBot-master.zip')
+            print("Closing Program Please Reload")
+            print(f'Updating from {Version} to {data}')
+            exit(0)
+        else:
+            print ('You chose no so continuing with outdated software')
+
 
 updateVersion()
